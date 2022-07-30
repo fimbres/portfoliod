34abdad15984d0274ee7ba1c1e351b7c663ee3d4
Isaac <ifimbres@uabc.edu.mx>
2022-07-30 00:30:43 -0700

    new home header component

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,13 +2,18 @@ import React, { FC } from "react";
 import { Link } from "gatsby";
 import { StaticImage } from "gatsby-plugin-image";
 
+import HomeHeader from "../components/HomeHeader/HomeHeader";
 import Seo from "../components/seo";
 import "../assets/scss/main.scss";
 
 const IndexPage: FC = () => (
   <>
     <Seo title="Home" />
-    <div className="bg-red-400 text-5xl font-bold">hola</div>
+    <HomeHeader />
+    <div className="h-screen"/>
+    <div className="py-24">
+      
+    </div>
   </>
 )
 
---
+++ src/types.d.ts
@@ -0,0 +1,21 @@
+// <reference types="react-scripts" />
+
+declare module '*.mp4' {
+    const src: string;
+    export default src;
+}
+
+declare module '*.pdf' {
+    const src: string;
+    export default src;
+}
+
+declare module '*.png' {
+    const src: string;
+    export default src;
+}
+
+declare module "*.svg" {
+    const content: any;
+    export default content;
+  }
