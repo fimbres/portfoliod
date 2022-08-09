85dcf1f4d0f0a78729931cf63cb0612a35732d55
Isaac <ifimbres@uabc.edu.mx>
2022-08-08 19:56:29 -0700

    new About Me section

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,12 +3,14 @@ import React, { FC } from "react";
 import HomeHeader from "../components/HomeHeader/HomeHeader";
 import Layout from "../components/Layout";
 import Seo from "../components/seo";
+import { AboutMe } from "../components/AboutMe";
 import "../assets/scss/main.scss";
 
 const IndexPage: FC = () => (
   <Layout>
     <Seo title="Home" />
     <HomeHeader />
+    <AboutMe />
   </Layout>
 )
 
