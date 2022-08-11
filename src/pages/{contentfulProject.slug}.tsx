b7c02cc87b65a340fee84036d2d1f4002502929a
Isaac <ifimbres@uabc.edu.mx>
2022-08-10 22:55:24 -0700

    updating the template of projects

--- src/pages/{contentfulProject.slug}.tsx
+++ src/pages/{contentfulProject.slug}.tsx
@@ -1,7 +1,45 @@
-import React from 'react';
+import React, { FC } from 'react';
+import { graphql } from 'gatsby';
+import { GatsbyImage, getImage } from 'gatsby-plugin-image';
+import Layout from '../components/Layout';
 
-export const Template = () => {
+export const query = graphql`
+  query ($title: String) {
+    contentfulProject(title: {eq: $title}) {
+      carousselImage {
+        gatsbyImage(width: 300)
+      }
+      codeUrl {
+        raw
+      }
+      features {
+        feature
+      }
+      id
+      images {
+        gatsbyImage(width: 300)
+        description
+      }
+      longDescription {
+        longDescription
+      }
+      shortDescription
+      slug
+      title
+      technologies {
+        percentage
+        technology
+      }
+    }
+  }
+`;
+
+const Template: FC = ({ data }) => {
   return (
-    <div>Template</div>
+    <Layout>
+      <div className='mt-56'>{data.contentfulProject.shortDescription}</div>
+    </Layout>
   )
 }
+
+export default Template;
