c448c4e8b4cc5292c5248408a0655415124740a0
Isaac <ifimbres@uabc.edu.mx>
2022-08-10 21:05:33 -0700

    updating 404 page

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,11 +1,21 @@
 import React, { FC } from "react";
+import { Link } from "gatsby";
 
 import Seo from "../components/seo";
+import Layout from "../components/Layout";
+import "../assets/scss/pages.scss";
 
 const NotFoundPage: FC = () => (
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+    <Layout>
+        <div className="bg-white dark:bg-neutral-800">
+            <div className="page">
+                <div className="page__title">Page not found! :(</div>
+                <Link className="page__button" to="/">Go to Home Page</Link>
+            </div>
+        </div>
+    </Layout>
 )
 
-export const Head = () => <Seo title="404: Not Found" />;
+export const Head = () => <Seo title="Not Found" />;
 
 export default NotFoundPage;
