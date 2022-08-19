834a7cc32fb232b9f9d95ca2014dddf2b890eb1c
Isaac <ifimbres@uabc.edu.mx>
2022-08-18 23:59:33 -0700

    minor changes

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,7 +2,7 @@ import React, { FC } from "react";
 
 import HomeHeader from "../components/HomeHeader/HomeHeader";
 import Layout from "../components/Layout";
-import Seo from "../components/seo";
+import Seo from "../components/Seo";
 import { AboutMe } from "../components/AboutMe";
 import { Skills } from "../components/Skills";
 import { Projects } from "../components/Projects";
