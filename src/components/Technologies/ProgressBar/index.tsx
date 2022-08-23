8cc9ea3d9cc3024ff233987a7ade8e7e404bd4ef
Isaac <ifimbres@uabc.edu.mx>
2022-08-23 13:04:38 -0700

    fixing bugs in progressbar

--- src/components/Technologies/ProgressBar/index.tsx
+++ src/components/Technologies/ProgressBar/index.tsx
@@ -16,11 +16,11 @@ const ProgressBar: FC<ProgressBarProps> = ({ percentage, fromColor, toColor }) =
             <div className='progress-bar__card'/>
             <div className='progress-bar__progress'>
                 <svg width="149" height="149" viewBox="0 0 149 149" fill="none" xmlns="http://www.w3.org/2000/svg">
-                    <rect x="10" y="10" width="129" height="129" rx="64.5" stroke={`url(#paint0_angular_109_${toColor})`} stroke-width="20" strokeDashoffset={calculateProgress}/>
+                    <rect x="10" y="10" width="129" height="129" rx="64.5" stroke={`url(#paint0_angular_109_${toColor})`} strokeWidth="20" strokeDashoffset={calculateProgress}/>
                     <defs>
                         <radialGradient id={`paint0_angular_109_${toColor}`} cx="0" cy="0" r="1" gradientUnits="userSpaceOnUse" gradientTransform="translate(75 74) rotate(90) scale(74.5)">
-                            <stop offset="3.4895e-05" stop-color={fromColor}/>
-                            <stop offset="1" stop-color={toColor}/>
+                            <stop offset="3.4895e-05" stopColor={fromColor}/>
+                            <stop offset="1" stopColor={toColor}/>
                         </radialGradient>
                     </defs>
                 </svg>
