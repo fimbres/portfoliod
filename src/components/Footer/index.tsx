05f2c55e66334f01676cc36c3aadbc53da4dc55d
Isaac <fimbresisaac23@gmail.com>
2023-01-16 13:12:52 -0800

    fixing bug in hover animation

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -8,7 +8,7 @@ const Footer: FC = () => {
     <div className='footer'>
       <div className='footer__text normal'>This portfolio was developed using Sass, Tailwind and Gatsby with TypeScript.</div>
       <div className='footer__inner'>
-        <div className='footer__text'>© {new Date().getFullYear()}, All Rights Reserved Fimbres Technologies</div>
+        <div className='footer__text'>© {new Date().getFullYear()} Fimbres Technologies.</div>
         <div className='footer__links'>
           <a href="https://github.com/fimbres" rel="noreferrer" target="_blank"><FaGithubSquare size={35} /></a>
           <a href="https://www.linkedin.com/in/fimbres-isaac/" rel="noreferrer" target="_blank"><FaLinkedin size={35} /></a>
